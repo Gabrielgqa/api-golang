f9f7338de738fa6e7256ac433782ef3f0fc27584
GabrielGQA <gabrielgqa@gmail.com>
2023-08-21 22:36:41 -0300

    feat: server up

--- cmd/api-golang/main.go
+++ cmd/api-golang/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/gabrielgqa/api-golang/internal/server"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	serv, err := server.New("3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go serv.Start()
+
+	// Wait for an in interrupt.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	serv.Close()
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,7 +1,40 @@
 package server
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi"
+)
 
 type Server struct {
 	server *http.Server
+}
+
+// New inicialize a new server with configuration.
+func New(port string) (*Server, error) {
+	r := chi.NewRouter()
+
+	serv := &http.Server{
+		Addr:         ":" + port,
+		Handler:      r,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	server := Server{server: serv}
+
+	return &server, nil
+}
+
+// Close server resources.
+func (serv *Server) Close() error {
+	return nil
+}
+
+// Start the server.
+func (serv *Server) Start() {
+	log.Printf("Server running on http://localhost%s", serv.server.Addr)
+	log.Fatal(serv.server.ListenAndServe())
 }
